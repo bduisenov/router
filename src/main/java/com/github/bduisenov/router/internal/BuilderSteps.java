4e57bda05ce28d49577dca1de28c410953460fd4
Sorokin Andrey <sorokin1990@gmail.com>
2021-02-13 09:08:29 +0100

    - couple of architectural problems identified

--- src/main/java/com/github/bduisenov/router/internal/BuilderSteps.java
+++ src/main/java/com/github/bduisenov/router/internal/BuilderSteps.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.function.Function;
 
-public abstract class BuilderSteps {
+public final class BuilderSteps {
 
     private BuilderSteps() {
         //NOOP
